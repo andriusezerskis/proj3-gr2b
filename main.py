55a9ee85c56a08953771f0c0ab27ee7cf827a2c9
moiravs <moira.vanderslagmolen@ulb.be>
2023-11-17 21:00:00 +0100

    [ADD] initial docstring

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Project 3: Ecosystem simulation in 2D
+Authors: Loïc Bloomaert, Hà Ûyen Tran, Andrius Ezerskis, Mathieu Vannimmen, Moïra Vanderslagmolen
+Date: December 2023
+"""
+
+
+def main():
+    print("")
