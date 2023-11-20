abb37ab88b86f17384ade15bfd0d436e4b7df13c
XEL0 <axelyx@gmail.com>
2023-11-20 18:11:43 +0100

    :sparkles: PyQT screen

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 """
 Project 3: Ecosystem simulation in 2D
-Authors: Loïc Bloomaert, Hà Ûyen Tran, Andrius Ezerskis, Mathieu Vannimmen, Moïra Vanderslagmolen
+Authors: Loïc Blommaert, Hà Ûyen Tran, Andrius Ezerskis, Mathieu Vannimmen, Moïra Vanderslagmolen
 Date: December 2023
 """
+
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
 
 
 def main():
     print("")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle('Simulation 2D')
+    window.setGeometry(100, 100, 400, 300)
+    window.show()
+    sys.exit(app.exec())
