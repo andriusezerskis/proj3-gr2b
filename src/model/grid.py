7a9f1518e28f2b4332b266707b70821ed18cc637
andriusezerskis <andrius.ezerskis@ulb.be>
2023-11-26 00:40:27 +0100

    [ADD] adjacent cases entity check

--- src/model/grid.py
+++ src/model/grid.py
@@ -6,15 +6,29 @@ class Grid:
         self.size = size
         
     def initialize(self):
+        """Probably useless, need random initialization"""
         for line in range(self.size[0]):
             self.cases.append([])
             for col in range(self.size[1]):
                 self.cases[line].append(Case([]))
                 
-    def step(self):
-        for row in self.cases:
-            for case in row:
-                # if two entities are on the same case, they interact
-                # get both entities
-                # apply relation
-                ...
+        # add entities randomly TODO
+                
+    def entityInAdjacentCase(self, entity, currentCase):
+        """Checks if, given a current case, there's an entity in an adjacent case to eventually interact with"""
+        adjacent_cases = [
+            (currentCase[0] - 1, currentCase[1]),  # up
+            (currentCase[0] + 1, currentCase[1]),  # down
+            (currentCase[0], currentCase[1] - 1),  # left
+            (currentCase[0], currentCase[1] + 1),  # right
+            (currentCase[0] - 1, currentCase[1] - 1),  # upper left
+            (currentCase[0] - 1, currentCase[1] + 1),  # upper right
+            (currentCase[0] + 1, currentCase[1] - 1),  # lower left
+            (currentCase[0] + 1, currentCase[1] + 1)  # lower right
+        ]
+        
+        for case in adjacent_cases:
+            if 0 <= case[0] < self.size[0] and 0 <= case[1] < self.size[1]:
+                if entity in self.cases[case[0]][case[1]].entities:
+                    return True
+        return False
