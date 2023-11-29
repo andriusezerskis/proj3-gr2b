5197868e27f2371ab6e2355e087c20051e058f37
andriusezerskis <andrius.ezerskis@ulb.be>
2023-11-29 13:30:15 +0100

    [ADD] method to move an entity on the grid

--- src/model/grid.py
+++ src/model/grid.py
@@ -32,3 +32,10 @@ class Grid:
                         self.tiles[tile[0]][tile[1]].getEntity())
 
         return entitiesList
+
+
+    def moveEntity(self, entity, currentTile, nextTile):
+        """Moves an entity from a tile to another"""
+        if not self.tiles[nextTile[0]][nextTile[1]].hasEntity():
+            self.tiles[nextTile[0]][nextTile[1]].addEntity(entity)
+            self.tiles[currentTile[0]][currentTile[1]].removeEntity()
