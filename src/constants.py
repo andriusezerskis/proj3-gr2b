c6cd87f133d8c29046a8f8c263b0052a34c94244
XEL0 <axelyx@gmail.com>
2023-12-04 21:10:18 +0100

    :pencil2: fixed typos in dosctring

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 """
 Project 3: Ecosystem simulation in 2D
-Authors: Loïc Bloomaert, Hà Ûyen Tran, Andrius Ezerskis, Mathieu Vannimmen, Moïra Vanderslagmolen
+Authors: Loïc Blommaert, Hà Uyên Tran, Andrius Ezerskis, Mathieu Vannimmen, Moïra Vanderslagmolen
 Date: December 2023
 """
 
