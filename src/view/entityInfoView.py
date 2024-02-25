ce79244afa8028a802d3abfb422729888146ebc0
Andrius Ežerskis <92785029+andriusezerskis@users.noreply.github.com>
2024-02-25 19:05:20 +0100

    Entity info health bar and ability to inflict damage (#16)
    * :twisted_rightwards_arrows: merge main into entity-info
    
    * :sparkles: added health points to json file
    
    * :sparkles::lipstick: entity health bar now displayed + fixed aesthetic
    
    * :art::recycle: entity health points
    
    * :construction: trying to get health points to work, player causing crash
    
    * :art::sparkles: damage and health points implemented

--- src/view/entityInfoView.py
+++ src/view/entityInfoView.py
@@ -8,7 +8,8 @@ from PyQt6.QtWidgets import QDockWidget,  QVBoxLayout, QLabel, QProgressBar, QPu
 from controller.gridController import GridController
 from model.entities.entity import Entity
 from model.entities.animal import Animal
-from constants import CONTROL_PLAYER, ENTITY_DEAD_MESSAGE, ENTITY_MAX_HUNGER, ENTITY_NOT_SELECTED, ENTITY_PARAMETERS, RELEASE_PLAYER
+from constants import CONTROL_PLAYER, ENTITY_DEAD_MESSAGE, ENTITY_MAX_HUNGER, ENTITY_NOT_SELECTED, \
+    ENTITY_PARAMETERS, RELEASE_PLAYER, HEALTH_BAR_TEXT, NAME_TEXT, AGE_TEXT, HUNGER_TEXT
 
 
 class EntityInfoView(QDockWidget):
@@ -27,13 +28,13 @@ class EntityInfoView(QDockWidget):
         self.controlButton.clicked.connect(self.controlEntity)
         self.controlButton.hide()
 
-        self.lageButton = QPushButton(RELEASE_PLAYER)
-        self.lageButton.clicked.connect(self.controlEntity)
-        self.lageButton.hide()
+        self.releaseButton = QPushButton(RELEASE_PLAYER)
+        self.releaseButton.clicked.connect(self.controlEntity)
+        self.releaseButton.hide()
 
         self.buttonLayout = QHBoxLayout()
         self.buttonLayout.addWidget(self.controlButton)
-        self.buttonLayout.addWidget(self.lageButton)
+        self.buttonLayout.addWidget(self.releaseButton)
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.healthBar)
@@ -56,15 +57,15 @@ class EntityInfoView(QDockWidget):
         """Shows information about an entity"""
         self.entity = entity
         self.controlButton.show()
-        self.lageButton.show()
+        self.releaseButton.show()
 
         self.healthBar.show()
-        self.healthBar.setValue(int(entity.getHealthPoints()))
         self.healthBar.setRange(0, entity.getMaxHealthPoints())
-        self.healthBar.setFormat("Santé")
+        self.healthBar.setValue(int(entity.getHealthPoints()))
+        self.healthBar.setFormat(f"{HEALTH_BAR_TEXT}{int(entity.getHealthPoints())}")
 
-        baseText = f"Prénom: {entity.getName()}\n"
-        baseText += f"Âge: {entity.getDisplayAge()} jours\n"
+        baseText = f"{NAME_TEXT}{entity.getName()}\n"
+        baseText += f"{AGE_TEXT}{entity.getDisplayAge()} jours\n"
         if isinstance(entity, Animal):
             self.hungerBar.show()
             preys = entity.getPreysNames()
@@ -80,7 +81,7 @@ class EntityInfoView(QDockWidget):
                 i += 1
             baseText += "\n"
             baseText += f"{entity.getCount()} {ENTITY_PARAMETERS[entity.__class__.__name__]['french_name'].lower()}s\n"
-            self.hungerBar.setFormat("Faim")
+            self.hungerBar.setFormat(f"{HUNGER_TEXT}")
             self.hungerBar.setValue(int(entity.getHunger()))
         else:
             self.hungerBar.hide()
