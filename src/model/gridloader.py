b5d5878165199a6c1d8d71193dbb293968a83513
andriusezerskis <andrius.ezerskis@ulb.be>
2024-05-12 12:20:59 +0200

    :bug: fixed bug where entities were all at age 0 when loading saved map

--- src/model/gridloader.py
+++ src/model/gridloader.py
@@ -1,11 +1,13 @@
 from abc import ABC
 from typing import Type
+import random
 
 from model.grid import Grid
 from model.terrains.tile import Tile
 from model.generator.gridGenerator import GridGenerator
 from model.entities.entity import Entity
 from utils import Point, getTerminalSubclassesOfClass
+from parameters import EntityParameters
 
 
 class GridLoader(ABC):
@@ -50,7 +52,7 @@ class GridLoader(ABC):
                 for x, symbol in enumerate(f.readline().strip().split(" ")):
                     entityType = cls._getEntityTypeFromSymbol(symbol)
                     if entityType:
-                        mat[y][x].addNewEntity(entityType)
+                        mat[y][x].addNewEntity(entityType, random.randint(0, EntityParameters.MAX_AGE))
 
         grid = Grid(gridSize)
         grid.initialize(mat, GridGenerator.getIslands(mat, gridSize, (-1,)))
